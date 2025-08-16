1ca88f9d8024a986d91831c9dd14c80a9f6cee92
Jay Jadhao <jadhaojay190@gmail.com>
2025-08-17 02:21:44 +0530

    Jay(Team data)

--- prmitr-cisco/src/data/teamData.js
+++ prmitr-cisco/src/data/teamData.js
@@ -14,7 +14,7 @@ export const coreTeam = [
     name: "Vedant Himte",
     role: "Co-Lead",
     bio: "Leading Cisco Networking Academy with a vision for innovation.",
-    image: "/assets/team/core_vedant.jpg",
+    image: "/assets/team/Core_VedantHimte.jpg",
     links: {
       linkedin: "#",
       github: "#",
@@ -22,10 +22,34 @@ export const coreTeam = [
     }
   },
   {
-    name: "Vedant Himte",
-    role: "Co-Lead",
+    name: "Mohammad Mudassir",
+    role: "Tech Associate",
+    bio: "Leading Cisco Networking Academy with a vision for innovation.",
+    image: "/assets/team/Mudassir Mohammad.jpg",
+    links: {
+      linkedin: "#",
+      github: "#",
+      twitter: "#"
+    }
+  },
+  
+  {
+    name: "Khushi Khatale",
+    role: "PR Analyst",
     bio: "Leading Cisco Networking Academy with a vision for innovation.",
     image: "/assets/team/core_vedant.jpg",
+    links: {
+      linkedin: "#",
+      github: "#",
+      twitter: "#"
+    }
+  },
+  
+  {
+    name: "Danish Ahemad",
+    role: "Event Coordinator",
+    bio: "Leading Cisco Networking Academy with a vision for innovation.",
+    image: "/assets/team/Danish Ahemad.jpg",
     links: {
       linkedin: "#",
       github: "#",
@@ -37,8 +61,30 @@ export const coreTeam = [
 
 export const teamLeads = [
   {
-    name: "Vedant Himte",
+    name: "Jay Jadhao",
     role: "Web Development Lead",
+    bio: "Passionate about frontend and backend development.",
+    image: "/assets/team/Jay_Jadhao.jpg",
+    links: {
+      linkedin: "#",
+      github: "#",
+      twitter: "#"
+    }
+  },
+  {
+    name: "Riddhi Akhare",
+    role: "Management Team Lead",
+    bio: "Passionate about frontend and backend development.",
+    image: "/assets/team/RiddhiAkhare.jpeg",
+    links: {
+      linkedin: "#",
+      github: "#",
+      twitter: "#"
+    }
+  },
+  {
+    name: "Atharv Borkar",
+    role: "AI & Data Science Lead",
     bio: "Passionate about frontend and backend development.",
     image: "/assets/team/michael.jpg",
     links: {
@@ -46,13 +92,79 @@ export const teamLeads = [
       github: "#",
       twitter: "#"
     }
-  }
+  },
+  {
+    name: "Gunjan Pachpohar",
+    role: "Cybersecurity Lead",
+    bio: "Passionate about frontend and backend development.",
+    image: "/assets/team/michael.jpg",
+    links: {
+      linkedin: "#",
+      github: "#",
+      twitter: "#"
+    }
+  },
+  {
+    name: "Arth Gundapure",
+    role: "DSA Lead",
+    bio: "Passionate about frontend and backend development.",
+    image: "/assets/team/michael.jpg",
+    links: {
+      linkedin: "#",
+      github: "#",
+      twitter: "#"
+    }
+  },
+  {
+    name: "Apurva Kadu ",
+    role: "Networking Lead",
+    bio: "Passionate about frontend and backend development.",
+    image: "/assets/team/Apurva_Kadu.jpg",
+    links: {
+      linkedin: "#",
+      github: "#",
+      twitter: "#"
+    }
+  },
+  {
+    name: "Rahul Kharap",
+    role: "Creative Team Lead",
+    bio: "Passionate about frontend and backend development.",
+    image: "/assets/team/Rahul_Kharap.jpg",
+    links: {
+      linkedin: "#",
+      github: "#",
+      twitter: "#"
+    }
+  },
+  {
+    name: "Lajari Pawanikar",
+    role: "Creative Team Lead",
+    bio: "Passionate about frontend and backend development.",
+    image: "/assets/team/michael.jpg",
+    links: {
+      linkedin: "#",
+      github: "#",
+      twitter: "#"
+    }
+  },
 ];
 
 export const teamMembers = [
   {
-    name: "Emily Johnson",
-    role: "Member",
+    name: "Harshada Tijare",
+    role: "Web Develpment Member",
+    bio: "Interested in learning and contributing to networking projects.",
+    image: "/assets/team/HarshadaTijare.jpg",
+    links: {
+      linkedin: "#",
+      github: "#",
+      twitter: "#"
+    }
+  },
+  {
+    name: "Pratansha Chaudhary",
+    role: "Web Develpment Member",
     bio: "Interested in learning and contributing to networking projects.",
     image: "/assets/team/emily.jpg",
     links: {
@@ -62,10 +174,241 @@ export const teamMembers = [
     }
   },
   {
-    name: "David Lee",
-    role: "Member",
+    name: "Khushi Puri",
+    role: "AI & Data Science Member",
     bio: "Focuses on network automation and scripting.",
     image: "/assets/team/david.jpg",
+    links: {
+      linkedin: "#",
+      github: "#",
+      twitter: "#"
+    }
+  },
+  {
+    name: "Tanay Thakare",
+    role: "AI & Data Science Member",
+    bio: "Focuses on network automation and scripting.",
+    image: "/assets/team/david.jpg",
+    links: {
+      linkedin: "#",
+      github: "#",
+      twitter: "#"
+    }
+  },
+  {
+    name: "Parth Deshmukh",
+    role: "AI & Data Science Member",
+    bio: "Focuses on network automation and scripting.",
+    image: "/assets/team/ai_parth.jpg",
+    links: {
+      linkedin: "#",
+      github: "#",
+      twitter: "#"
+    }
+  },
+  {
+    name: "Parth Barad",
+    role: "AI & Data Science Member",
+    bio: "Focuses on network automation and scripting.",
+    image: "/assets/team/PARTHBARAD.jpg",
+    links: {
+      linkedin: "#",
+      github: "#",
+      twitter: "#"
+    }
+  },
+  {
+    name: "Tanvi Khatale",
+    role: "Cybersecurity Member",
+    bio: "Focuses on network automation and scripting.",
+    image: "/assets/team/david.jpg",
+    links: {
+      linkedin: "#",
+      github: "#",
+      twitter: "#"
+    }
+  },
+  {
+    name: "Khushi Awghad",
+    role: "Cybersecurity Member",
+    bio: "Focuses on network automation and scripting.",
+    image: "/assets/team/KhushiAwghad.jpg",
+    links: {
+      linkedin: "#",
+      github: "#",
+      twitter: "#"
+    }
+  },
+  {
+    name: "Gauri Bhasme",
+    role: "Cybersecurity Member",
+    bio: "Focuses on network automation and scripting.",
+    image: "/assets/team/GauriBhasme.jpg",
+    links: {
+      linkedin: "#",
+      github: "#",
+      twitter: "#"
+    }
+  },
+  {
+    name: "Samruddhi Gondane",
+    role: "Cybersecurity Member",
+    bio: "Focuses on network automation and scripting.",
+    image: "/assets/team/SamruddhiGondane.jpg",
+    links: {
+      linkedin: "#",
+      github: "#",
+      twitter: "#"
+    }
+  },
+  {
+    name: "Khushboo Thakur",
+    role: "Cybersecurity Member",
+    bio: "Focuses on network automation and scripting.",
+    image: "/assets/team/KhushbooThakur.jpg",
+    links: {
+      linkedin: "#",
+      github: "#",
+      twitter: "#"
+    }
+  },
+  {
+    name: "Harsh Deshmukh",
+    role: "DSA Member",
+    bio: "Focuses on network automation and scripting.",
+    image: "/assets/team/HarshDeshmukh.jpg",
+    links: {
+      linkedin: "#",
+      github: "#",
+      twitter: "#"
+    }
+  },
+  {
+    name: "Pranita Farsole",
+    role: "DSA Member",
+    bio: "Focuses on network automation and scripting.",
+    image: "/assets/team/PranitaFarsole.jpg",
+    links: {
+      linkedin: "#",
+      github: "#",
+      twitter: "#"
+    }
+  },
+  {
+    name: "Siddhi Wakade	",
+    role: "DSA Member",
+    bio: "Focuses on network automation and scripting.",
+    image: "/assets/team/SiddhiWakade.jpg",
+    links: {
+      linkedin: "#",
+      github: "#",
+      twitter: "#"
+    }
+  },
+  {
+    name: "Radha Waghmare",
+    role: "DSA Member",
+    bio: "Focuses on network automation and scripting.",
+    image: "/assets/team/RadhaWaghmare.jpg",
+    links: {
+      linkedin: "#",
+      github: "#",
+      twitter: "#"
+    }
+  },
+  {
+    name: "Nakul Indurkar",
+    role: "DSA Member",
+    bio: "Focuses on network automation and scripting.",
+    image: "/assets/team/IndurkarNakul.jpg",
+    links: {
+      linkedin: "#",
+      github: "#",
+      twitter: "#"
+    }
+  },
+  {
+    name: "	Om Kantale",
+    role: "DSA Member",
+    bio: "Focuses on network automation and scripting.",
+    image: "/assets/team/Om_Kantale.jpg",
+    links: {
+      linkedin: "#",
+      github: "#",
+      twitter: "#"
+    }
+  },
+  {
+    name: "Om Pusatkar",
+    role: "Networking Member",
+    bio: "Focuses on network automation and scripting.",
+    image: "/assets/team/OMPUSATKAR.jpg",
+    links: {
+      linkedin: "#",
+      github: "#",
+      twitter: "#"
+    }
+  },
+  {
+    name: "Yogeshwari Mutyalu",
+    role: "Networking Member",
+    bio: "Focuses on network automation and scripting.",
+    image: "/assets/team/YogeshwariMutyalu.png",
+    links: {
+      linkedin: "#",
+      github: "#",
+      twitter: "#"
+    }
+  },
+  {
+    name: "	Gauri Ghatol",
+    role: "Networking Member",
+    bio: "Focuses on network automation and scripting.",
+    image: "/assets/team/GauriGhatol.jpg",
+    links: {
+      linkedin: "#",
+      github: "#",
+      twitter: "#"
+    }
+  },
+  {
+    name: "Anushka Vishwakarma",
+    role: "Networking Member",
+    bio: "Focuses on network automation and scripting.",
+    image: "/assets/team/AnushkaVishwakarma.jpg",
+    links: {
+      linkedin: "#",
+      github: "#",
+      twitter: "#"
+    }
+  },
+  {
+    name: "Vaibhav Korde",
+    role: "Management Member",
+    bio: "Focuses on network automation and scripting.",
+    image: "/assets/team/VaibhavKorde.jpg",
+    links: {
+      linkedin: "#",
+      github: "#",
+      twitter: "#"
+    }
+  },
+  {
+    name: "Parth Jaiswal",
+    role: "Management Member",
+    bio: "Focuses on network automation and scripting.",
+    image: "/assets/team/ParthJaiswal.jpg",
+    links: {
+      linkedin: "#",
+      github: "#",
+      twitter: "#"
+    }
+  },
+  {
+    name: "Prasanna Patil",
+    role: "Management Member",
+    bio: "Focuses on network automation and scripting.",
+    image: "/assets/team/AnushkaVishwakarma.jpg",
     links: {
       linkedin: "#",
       github: "#",
