3c9a25ed900a2cea616987f9a7063b17e0375c9c
Vedant Himte <112320570+vedanthimte@users.noreply.github.com>
2025-08-20 03:51:34 +0000

    Version 3

--- prmitr-cisco/src/data/facultyData.js
+++ prmitr-cisco/src/data/facultyData.js
@@ -6,7 +6,7 @@ const facultyData = [
     link :"https://www.linkedin.com/in/prof-dr-chandrashekhar-deshmukh-67a9a121" 
   },
   {
-    name: "Prof Arpit Chaudhari",
+    name: "Prof. Arpit Chaudhari",
     role: "Cisco Co-Coordinator",
     image: "/assets/faculty/Arpit_Chaudhari.png",
     link :"https://www.linkedin.com/in/arpit-chaudhari-0720b5294/" 
@@ -18,28 +18,41 @@ const facultyData = [
     link :"www.linkedin.com/in/nupoor-yawale-37167633" 
   },
   {
-    name: "Prof Vaibhav J Babrekar",
+    name: "Prof. Vaibhav J Babrekar",
     role: "Faculty Coordinator",
     image: "/assets/faculty/vaibhav.jpg",
     link :"http://linkedin.com/in/vaibhav-babrekar-245524286" 
   },
   {
-    name: "Prof Neha A Kandalkar",
+    name: "Prof. Neha A Kandalkar",
     role: "Faculy Coordinator",
     image: "/assets/faculty/ai&ds_neha.jpg",
     link : "https://www.linkedin.com/in/neha-kandalkar-6b014b49"
   },
   {
-    name: "Prof Aniket V Deshmukh ",
+    name: "Prof. Aniket V Deshmukh ",
     role: "Faculy Coordinator",
     image: "/assets/faculty/Aniket.png",
     link : "https://www.linkedin.com/in/aniket-deshmukh-82a335a0"
   },
   {
-    name: "Prof Rutuja S Adhau",
+    name: "Prof. Rutuja S Adhau",
     role: "Faculty Coordinator",
     image: "/assets/faculty/rutuja.jpg",
     link : ""
+  },
+  {
+    name: "Prof. Samruddhi M. Inzalkar",
+    role: "Faculty Coordinator",
+    image: "/assets/faculty/samruddhitayade.png",
+    link : ""
+  }
+  ,
+  {
+    name: "Dr Karan S Belsare",
+    role: "Faculty Coordinator",
+    image: "/assets/faculty/Karan_Belsare.jpg",
+    link : "www.linkedin.com/in/dr-karan-belsare-52604636"
   }
 ];
 
